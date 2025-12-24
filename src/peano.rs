77e6e30859a0d01b5ab9275f7a40a346dce141a8
Laszlo Lukacs <lukacslacko@gmail.com>
2025-12-24 13:48:50 +0100

    do not quantify axioms

--- src/peano.rs
+++ src/peano.rs
@@ -6,66 +6,37 @@ pub fn axioms() -> UnitResult {
     let var_y = make_numeric_variable("Y".to_string())?;
     let zero = make_numeric_constant_zero("0".to_string())?;
 
-    make_quantify(
-        var_x.clone(),
-        make_not(make_equals(zero.clone(), make_successor(var_x.clone())?)?)?,
-    )?
-    .assert_axiom(Name("peano 1"))?;
+    make_not(make_equals(zero.clone(), make_successor(var_x.clone())?)?)?
+        .assert_axiom(Name("peano 1"))?;
 
-    make_quantify(
-        var_x.clone(),
-        make_quantify(
-            var_y.clone(),
-            make_imply(
-                make_equals(
-                    make_successor(var_x.clone())?,
-                    make_successor(var_y.clone())?,
-                )?,
-                make_equals(var_x.clone(), var_y.clone())?,
-            )?,
+    make_imply(
+        make_equals(
+            make_successor(var_x.clone())?,
+            make_successor(var_y.clone())?,
         )?,
+        make_equals(var_x.clone(), var_y.clone())?,
     )?
     .assert_axiom(Name("peano 2"))?;
 
-    make_quantify(
-        var_x.clone(),
-        make_equals(make_add(var_x.clone(), zero.clone())?, var_x.clone())?,
-    )?
-    .assert_axiom(Name("peano 3"))?;
+    make_equals(make_add(var_x.clone(), zero.clone())?, var_x.clone())?
+        .assert_axiom(Name("peano 3"))?;
 
-    make_quantify(
-        var_x.clone(),
-        make_quantify(
-            var_y.clone(),
-            make_equals(
-                make_add(var_x.clone(), make_successor(var_y.clone())?)?,
-                make_successor(make_add(var_x.clone(), var_y.clone())?)?,
-            )?,
-        )?,
+    make_equals(
+        make_add(var_x.clone(), make_successor(var_y.clone())?)?,
+        make_successor(make_add(var_x.clone(), var_y.clone())?)?,
     )?
     .assert_axiom(Name("peano 4"))?;
 
-    make_quantify(
-        var_x.clone(),
-        make_equals(make_multiply(var_x.clone(), zero.clone())?, zero.clone())?,
-    )?
-    .assert_axiom(Name("peano 5"))?;
-    make_quantify(
-        var_x.clone(),
-        make_quantify(
-            var_y.clone(),
-            make_equals(
-                make_multiply(var_x.clone(), make_successor(var_y.clone())?)?,
-                make_add(
-                    make_multiply(var_x.clone(), var_y.clone())?,
-                    var_x.clone(),
-                )?,
-            )?,
-        )?,
+    make_equals(make_multiply(var_x.clone(), zero.clone())?, zero.clone())?
+        .assert_axiom(Name("peano 5"))?;
+
+    make_equals(
+        make_multiply(var_x.clone(), make_successor(var_y.clone())?)?,
+        make_add(make_multiply(var_x.clone(), var_y.clone())?, var_x.clone())?,
     )?
     .assert_axiom(Name("peano 6"))?;
 
-    make_quantify(var_x.clone(), make_equals(var_x.clone(), var_x.clone())?)?
+    make_equals(var_x.clone(), var_x.clone())?
         .assert_axiom(Name("reflexivity"))?;
 
     Ok(())
@@ -112,15 +83,9 @@ pub fn eq_subs(
     if !matches!(y.get_kind(), NumericVariable | NumericConstant) {
         Err("eq_subs")?
     }
-    make_quantify(
-        x.clone(),
-        make_quantify(
-            y.clone(),
-            make_imply(
-                make_equals(x.clone(), y.clone())?,
-                make_imply(A.clone(), A.substitute(x, y)?)?,
-            )?,
-        )?,
+    make_imply(
+        make_equals(x.clone(), y.clone())?,
+        make_imply(A.clone(), A.substitute(x, y)?)?,
     )?
     .assert_axiom(Name("indiscernibility of identicals"))
 }
